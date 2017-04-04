13b6b1b714311dc98fb0619488b40762e8cd82c4
rx8 <rx8@duke.edu>
2017-04-03 20:21:25 -0400

    Complete Python codes to automatically capture several images with user defined delay

--- software/save-jpg.py
+++ software/save-jpg.py
@@ -1,7 +1,30 @@
 import urllib.request
 import random
+import time
 
-seed = random.random()
-print(seed)
 
-urllib.request.urlretrieve("http://10.146.13.107/capture?t="+str(seed), "test.jpg")
+def hello():
+    print("hello")
+
+
+def capture(path):
+    seed = random.random()
+    # print(seed)
+    urllib.request.urlretrieve("http://10.146.13.107/capture?t=" + str(seed),
+                               path)
+
+
+if __name__ == "__main__":
+    n = 2
+    print("Start captureing " + str(n) + " 5MP images...")
+
+    for x in range(0, n):
+        path = "Mon tests/img" + str(n) + ".jpg"
+        capture(path)
+        end = time.clock()
+        print("exe time: " + str(end))
+
+        print("sleep for 5 secs")
+        time.sleep(5)
+
+    print("Done")
