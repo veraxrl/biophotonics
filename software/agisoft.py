5f32fec9502ce0715c5ab8ae231d0b4eb9fef4da
DanielHull <danielhull13@gmail.com>
2017-04-24 13:59:27 -0400

    Readying for final integration

--- software/agisoft.py
+++ software/agisoft.py
@@ -1,25 +1,25 @@
 import PhotoScan
 
-path = "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/res.ply"
+path = "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/res.ply"
 
 
 def reconstruct():
     doc = PhotoScan.app.document
     chunk = doc.addChunk()
-    chunk.addPhotos(["C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img0.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img1.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img2.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img3.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img4.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img5.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img6.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img7.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img8.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img9.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img10.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img11.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img12.jpg",
-                     "C:/Users/Ruolan Xu/Desktop/BME 436/biophotonics/software/fetch/img13.jpg"])
+    chunk.addPhotos(["C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img0.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img1.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img2.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img3.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img4.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img5.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img6.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img7.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img8.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img9.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img10.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img11.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img12.jpg",
+                     "C:/Users/Daniel Hull/Documents/Projects/BFPD/software/fetch/img13.jpg"])
     chunk.matchPhotos(accuracy=PhotoScan.MediumAccuracy,
                       preselection=PhotoScan.GenericPreselection)
     chunk.alignCameras()
